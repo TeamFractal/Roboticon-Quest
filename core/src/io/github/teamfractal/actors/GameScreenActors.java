750b628bf28a75356389c1373b21655dab4cc4ee
C Beddows <cb1423@pc667s.cs.york.ac.uk>
2017-02-01 10:58:36 +0000

    Added the treasure chest random event

--- core/src/io/github/teamfractal/actors/GameScreenActors.java
+++ core/src/io/github/teamfractal/actors/GameScreenActors.java
@@ -133,9 +133,15 @@ public class GameScreenActors {
 				if (selectedPlot.hasOwner()) {
 					return;
 				}
-
+		 
 				Player player = game.getPlayer();
 				if (player.purchaseLandPlot(selectedPlot)) {
+					//Added a random event where the player finds a chest containing money - Christian Beddows
+					if (RandomEvents.tileHasChest()){
+						int playerTreasure = RandomEvents.treasureChest(game);
+						stage.addActor(new MessagePopUp("You found a treasure chest!","On your new tile you "
+								+ "find a buried treasure chest containing " + Integer.toString(playerTreasure) + "money!"));
+					}
 					TiledMapTileLayer.Cell playerTile = selectedPlot.getPlayerTile();
 					playerTile.setTile(screen.getPlayerTile(player));
 					textUpdate();
@@ -202,7 +208,7 @@ public class GameScreenActors {
 							///// Modified by Josh Neil - 
 							///// Added if else branches previously was just the instructions in the 
 							///// else branch followed by textUpdate()
-							if(RandomEvents.RoboticonIsFaulty()){
+							if(RandomEvents.roboticonIsFaulty()){
 								// Roboticon was faulty and has broken (cannot be placed)
 								game.getPlayer().removeRoboticon(roboticon);
 								stage.addActor(new MessagePopUp("That roboticon was faulty","That roboticon was faulty and exploded!"));
