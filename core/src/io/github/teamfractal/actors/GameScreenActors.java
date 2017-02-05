d935443d70622a12a9ebdfc49a65d463a07528b2
bp713 <bp713@york.ac.uk>
2017-02-05 18:51:38 +0000

    Add files via upload

--- core/src/io/github/teamfractal/actors/GameScreenActors.java
+++ core/src/io/github/teamfractal/actors/GameScreenActors.java
@@ -142,6 +142,12 @@ public class GameScreenActors {
 						stage.addActor(new MessagePopUp("You found a treasure chest!","On your new tile you "
 								+ "find a buried treasure chest containing " + Integer.toString(playerTreasure) + " money!"));
 					}
+					//Added a random event where you disturb a flock of geese on a plot
+					if (RandomEvents.GeeseAttack()){
+						int food = RandomEvents.Geese(game);
+						stage.addActor(new MessagePopUp("Disturbed a flock of Geese!","On your new tile you "
+								+ "discover a flock of geese they attack!, you lost " + Integer.toString(food) + " food!"));
+					}
 					TiledMapTileLayer.Cell playerTile = selectedPlot.getPlayerTile();
 					playerTile.setTile(screen.getPlayerTile(player));
 					textUpdate();
