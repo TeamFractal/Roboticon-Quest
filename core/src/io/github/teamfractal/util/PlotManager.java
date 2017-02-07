f473579a341754d75b49bff427f2b325ddcdf8cf
Jorgen <oliverdolan@msn.com>
2017-02-07 23:04:06 +0000

    Clean up code. Add methods to check for winner in RoboticonQuest

--- core/src/io/github/teamfractal/util/PlotManager.java
+++ core/src/io/github/teamfractal/util/PlotManager.java
@@ -69,6 +69,21 @@ public class PlotManager {
 
 		return p;
 	}
+	
+	public int getNumUnownedTiles() {
+		int numUnownedTiles = 0;
+		
+		for (LandPlot[] plotColumns : plots) {
+			for(LandPlot plot : plotColumns){
+				if(!plot.hasOwner()){
+					numUnownedTiles ++;
+				}
+			}
+		}
+		
+		return numUnownedTiles;
+	}
+	
 	/**
 	 * Creates a landplot from a tile tile on the tiled map
 	 * @param x - x coordinate on tiled map
@@ -104,7 +119,6 @@ public class PlotManager {
 			energy = 2;
 			food = 2;
 		}
-
 
 		LandPlot p = new LandPlot(ore, energy, food);
 		p.setupTile(this, x, y);
